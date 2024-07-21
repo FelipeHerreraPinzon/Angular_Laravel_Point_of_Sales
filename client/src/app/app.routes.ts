bd788e7093193a568384575a6458c966298c4c31
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2024-07-21 01:47:22 -0500

    api conectada !!!

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { IndexComponent} from './categories/index/index.component';
+import { CreateComponent} from './categories/create/create.component';
+
 
 export const routes: Routes = [
   { path: 'categorias', component: IndexComponent },
+  { path: 'crear', component: CreateComponent },
  
 ];
 
