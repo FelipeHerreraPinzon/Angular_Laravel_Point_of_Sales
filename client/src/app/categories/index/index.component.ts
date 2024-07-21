a485a974d5fed3bd0172ddb5da19e2ea9d4a4a4d
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2024-07-21 01:00:08 -0500

    client y server terminado

--- client/src/app/categories/index/index.component.ts
+++ client/src/app/categories/index/index.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-
+  
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CategoryService } from '../category.service';
+import { Category } from '../category';
+  
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
-
+  
+  categories: Category[] = [];
+      
+  /*------------------------------------------
+  --------------------------------------------
+  Created constructor
+  --------------------------------------------
+  --------------------------------------------*/
+  constructor(public categoryService: CategoryService) { }
+      
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  ngOnInit(): void {
+    this.categoryService.getAll().subscribe((data: Category[])=>{
+      this.categories = data;
+      console.log(this.categories);
+    })  
+  }
+      
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  deletePost(id:number){
+    this.categoryService.delete(id).subscribe(res => {
+         this.categories = this.categories.filter(item => item.id !== id);
+         console.log('Post deleted successfully!');
+    })
+  }
+  
 }
