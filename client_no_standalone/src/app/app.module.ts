630f2e262be65fb735256cb1894973a3bfdc0fa8
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2024-07-22 15:10:47 -0500

    interface y service

--- client_no_standalone/src/app/app.module.ts
+++ client_no_standalone/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+ // Asegúrate de que la ruta es correcta
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
--- client_no_standalone/src/app/categories/category.service.ts
+++ client_no_standalone/src/app/categories/category.service.ts
@@ -1,9 +1,107 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+
+import { Category } from './category';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  private apiURL = "http://127.0.0.1:8000/api";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.httpClient.get(this.apiURL + '/categorias/')
+      .pipe(
+        catchError(this.errorHandler)
+      )
+  }
+
+  create(post: Category): Observable<any> {
+    return this.httpClient.post(this.apiURL + '/categorias/', JSON.stringify(post), this.httpOptions)
+      .pipe(
+        catchError(this.errorHandler)
+      )
+  }
+
+  find(id: number): Observable<any> {
+    return this.httpClient.get(this.apiURL + '/categorias/' + id)
+      .pipe(
+        catchError(this.errorHandler)
+      )
+  }
+
+  update(id: number, post: Category): Observable<any> {
+    return this.httpClient.put(this.apiURL + '/categorias/' + id, JSON.stringify(post), this.httpOptions)
+      .pipe(
+        tap(() => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Actualizado',
+            text: 'El post ha sido actualizado correctamente',
+            timer: 2000,
+            timerProgressBar: true,
+          });
+        }),
+        catchError(this.errorHandler)
+      );
+  }
+
+  delete(id: number): Observable<any> {
+    return new Observable(observer => {
+      Swal.fire({
+        title: '¿Estás seguro?',
+        text: "¡No podrás revertir esto!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, eliminarlo!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.httpClient.delete(this.apiURL + '/categorias/' + id, this.httpOptions)
+            .pipe(
+              catchError(this.errorHandler)
+            ).subscribe(
+              data => {
+                observer.next(data);
+                observer.complete();
+                Swal.fire(
+                  '¡Eliminado!',
+                  'El post ha sido eliminado.',
+                  'success'
+                )
+              },
+              error => {
+                observer.error(error);
+              }
+            );
+        } else {
+          observer.complete();
+        }
+      });
+    });
+  }
+
+  errorHandler(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
 }
